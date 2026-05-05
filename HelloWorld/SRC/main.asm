702dfc9739bfebb37968f581b0cdc6f8d331db3f
Krzysztof Krystian Jankowski <kj@p1x.in>
2026-05-05 17:29:34 +0200

    terrain gen + readme

--- HelloWorld/SRC/main.asm
+++ HelloWorld/SRC/main.asm
@@ -15,10 +15,22 @@ DEF DPAD_LEFT_BIT                       EQU $0001
 DEF DPAD_UP_BIT                         EQU $0002
 DEF DPAD_DOWN_BIT                       EQU $0003
 DEF SPRITE_TILE_ADDR                    EQU $8000
-DEF SPRITE_X_CENTER                     EQU 80 + 8
-DEF SPRITE_Y_CENTER                     EQU 72 + 16
-DEF PLAYER_ANIM_DELAY                   EQU 4
+DEF BG_TILE_ADDR                        EQU $8000
+DEF BG_MAP_ADDR                         EQU $9800
 
+DEF TREE_COUNT                          EQU 24
+DEF SPRITE_X_CENTER                     EQU 80 + 8
+DEF SPRITE_Y_CENTER                     EQU 72 + 8
+DEF PLAYER_ANIM_DELAY                   EQU 4
+DEF TREE_FIRST_SLOT                     EQU 0
+DEF PLAYER_SLOT                         EQU TREE_COUNT + 5
+DEF PLAYER_OAM_ADDR                     EQU _OAMRAM + PLAYER_SLOT * 4
+
+DEF TREE_TILE_A                         EQU 2
+DEF TREE_TILE_B                         EQU 3
+DEF TILES_BG_INDEX_START                EQU 4
+DEF PAL_TREE                            EQU 0
+DEF PAL_GROUND                          EQU 0
 ; ==========================================================================|80|
 
 ; ======================================> HEADER DATA <=====================|80|
@@ -30,7 +42,8 @@ SECTION "Header", ROM0[$100]
 
 ; ======================================> WRAM DATA <=======================|80|
 SECTION "WRAM Data", WRAM0
-
+RandomSeed:
+  ds 1
 FrameCounter:
   ds 1
 PlayerAnimTimer:
@@ -48,6 +61,10 @@ Entry:
 
   xor a
   ld [rLCDC], a
+
+  ld a, [rDIV]
+  or $01                                ; avoid zero
+  ld [RandomSeed], a
 
   xor a
   ld [FrameCounter], a
@@ -83,12 +100,17 @@ Entry:
     ld b, CGBObjPalettesEnd - CGBObjPalettes
     call LoadObjPalettesCGB
 
+  .load_bg_palettes
+    ld hl, CGBBgPalettes
+    ld b, CGBBgPalettesEnd - CGBBgPalettes
+    call LoadBgPalettesCGB
+
+  call GenerateTerrain
   call InitPlayer
-  call InitTerrain
+  call GenerateForest
 
   .turn_lcd_on
-  ; LCD on, sprites on
-  ld a, $80 | $02
+  ld a, LCDCF_ON | LCDCF_OBJON | LCDCF_BGON | LCDCF_BG8000 | LCDCF_BG9800
   ld [rLCDC], a
 
 ; ======================================> MAIN LOOP <=======================|80|
@@ -108,12 +130,24 @@ WaitVBlank:
   ld a, [rLY]
   cp 144
   jr c, .wait_vblank
-
   .wait_vblank_end
     ld a, [rLY]
     cp 144
     jr nc, .wait_vblank_end
+  ret
 
+RandomByte:
+  ; Very simple 8-bit pseudo-random generator
+  ld a, [RandomSeed]
+  ld b, a
+
+  ld a, [rDIV]
+  xor b
+  add $3D
+  xor $A7
+  rlca
+
+  ld [RandomSeed], a
   ret
 
 LoadBytes:
@@ -133,6 +167,21 @@ LoadBytes:
   dec bc
   jr .copy
 ret
+
+LoadBgPalettesCGB:
+  ld a, $80
+  ld [rBGPI], a
+
+  .copy
+    ld a, b
+    or a
+    ret z
+
+    ld a, [hli]
+    ld [rBGPD], a
+    dec b
+    jr .copy
+  ret
 
 LoadObjPalettesCGB:
   ; Loads CGB OBJ palette bytes using auto-increment.
@@ -176,6 +225,12 @@ LoadObjPaletteCGB:
   jr .copy
 ret
 
+WaitForAnyDPad:
+  .wait
+    call ReadDPad
+    cp %00001111       ; no d-pad pressed
+    jr z, .wait
+  ret
 
 HandleDPad:
   call ReadDPad
@@ -198,22 +253,22 @@ ReadDPad:
   ret
 
 MoveRight:
-  ld hl, _OAMRAM + 1  ; sprite X
+  ld hl, PLAYER_OAM_ADDR + 1  ; sprite X
   inc [hl]
   ret
 
 MoveLeft:
-  ld hl, _OAMRAM + 1
+  ld hl, PLAYER_OAM_ADDR + 1
   dec [hl]
   ret
 
 MoveUp:
-  ld hl, _OAMRAM      ; sprite Y
+  ld hl, PLAYER_OAM_ADDR      ; sprite Y
   dec [hl]
   ret
 
 MoveDown:
-  ld hl, _OAMRAM
+  ld hl, PLAYER_OAM_ADDR
   inc [hl]
   ret
 
@@ -234,14 +289,14 @@ AnimatePlayer:
     ld [PlayerAnimFrame], a
 
   .apply_tile:
-  ld hl, _OAMRAM + 2
+  ld hl, PLAYER_OAM_ADDR + 2
   ld [hl], a
 
 .done
   ret
 
 InitPlayer:
-  ld hl, _OAMRAM                        ; sprite slot 0
+  ld hl, PLAYER_OAM_ADDR                ; sprite slot
   ld a, SPRITE_Y_CENTER                 ; Y pos
   ld [hli], a
   ld a, SPRITE_X_CENTER                 ; X pos
@@ -252,28 +307,69 @@ InitPlayer:
   ld [hli], a
   ret
 
-InitTerrain:
-  ld hl, _OAMRAM + 8                    ; sprite slot 2
-  ld a, SPRITE_Y_CENTER                 ; Y pos
-  ld [hli], a
-  ld a, SPRITE_X_CENTER - 16            ; X pos
-  ld [hli], a
-  ld a, 2                               ; tile
-  ld [hli], a
-  ld a, 0                               ; palette
-  ld [hli], a
+GenerateTerrain:
+  xor a
+  ld [rVBK], a
+  ld hl, BG_MAP_ADDR
+  ld bc, 32 * 32
 
-  ld hl, _OAMRAM + 12                   ; sprite slot 3
-  ld a, SPRITE_Y_CENTER - 16
-  ld [hli], a
-  ld a, SPRITE_X_CENTER + 24
-  ld [hli], a
-  ld a, 3                               ; tile id
-  ld [hli], a
-  ld a, 0                               ; palette
-  ld [hli], a
-ret
+  .next_tile
+    call RandomByte
+    and %00000011                       ; get 0..3
+    cp 3
+    jr nz, .valid
+    xor a                               ; convert 3 to 0
+  .valid
+    add a, TILES_BG_INDEX_START         ; convert to tiles IDs
+    ld [hli], a
 
+    dec bc
+    ld a, b
+    or c
+    jr nz, .next_tile
+
+    ret
+
+GenerateForest:
+  ld hl, _OAMRAM + TREE_FIRST_SLOT * 4
+  ld c, TREE_COUNT
+
+  .next_tree
+    ; random Y: 24..127
+    call RandomByte
+    and %01111111
+    add 24
+    ld [hli], a
+
+    ; random X: 8..167
+    call RandomByte
+    and %01111111
+    add 8
+    ld [hli], a
+
+    ; random tile: TREE_TILE_A or TREE_TILE_B
+    call RandomByte
+    and 1
+    jr z, .tile_a
+
+  .tile_b
+    ld a, TREE_TILE_B
+    jr .write_tile
+
+  .tile_a
+    ld a, TREE_TILE_A
+
+  .write_tile
+    ld [hli], a
+
+    ; palette 0
+    ld a, PAL_TREE
+    ld [hli], a
+
+    dec c
+    jr nz, .next_tree
+
+  ret
 ; ==========================================================================|80|
 
 ; ======================================> GAME DATA <=======================|80|
@@ -296,6 +392,14 @@ CGBObjPalette1:
 CGBObjPalette1End:
 CGBObjPalettesEnd:
 
+CGBBgPalettes:
+CGBBgPalette0:
+  dw $7FFF ; white
+  dw $03E0 ; green
+  dw $0200 ; dark green
+  dw $0100 ; very dark green
+CGBBgPalettesEnd:
+
 GameTiles:
 TilePlayerA:
   DB $00,$00,$42,$00,$18,$24,$66,$18
@@ -309,5 +413,14 @@ TileTree:
 TileTree2:
   DB $30,$08,$2E,$11,$DC,$03,$90,$66
   DB $10,$18,$60,$20,$10,$10,$10,$10
+TileGrass:
+  DB $00,$00,$00,$00,$22,$00,$00,$22
+  DB $88,$00,$00,$88,$22,$00,$00,$22
+TileDirt:
+  DB $AA,$00,$55,$00,$AA,$00,$55,$00
+  DB $AA,$00,$55,$00,$AA,$00,$55,$00
+TileWater:
+  DB $00,$00,$3C,$00,$00,$3C,$3C,$00
+  DB $00,$00,$3C,$00,$00,$3C,$3C,$00
 GameTilesEnd:
 ; ==========================================================================|80|
---
+++ README.md
@@ -0,0 +1,27 @@
+# GameBoy Color Projects
+
+
+## Game Engine
+This project is to create a simple engine and learn GBC processor instructions.
+
+## Hello, World Learning Project
+![Screenshot](HelloWorld/screenshot.png)
+
+Check [/HelloWorld/](HelloWorld) folder.
+
+## Tooling
+### Emulation
+- BGB [https://bgb.bircd.org/](bgb.bircd.org)
+- mGBA [https://mgba.io/](mgba.io)
+
+### Coding
+- RGBDS [https://rgbds.gbdev.io/](rgbds.gbdev.io)
+- Zed Editor [https://zed.dev/](zed.dev)
+
+### Graphics
+- Game Boy Tile Designer [https://www.devrs.com/gb/hmgd/gbtd.html](devrs.com/gb/hmgd/gbtd.html)
+- ProMotion NG [https://www.cosmigo.com/](cosmigo.com)
+- rgbgfx
+
+### Audio
+- hUGETracker [https://nickfa.ro/wiki/hUGETracker](nickfa.ro/wiki/hUGETracker)
