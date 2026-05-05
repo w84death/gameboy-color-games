04856d72c510d40ae5db7423e8b731590e083fda
Krzysztof Krystian Jankowski <kj@p1x.in>
2026-05-05 14:33:40 +0200

    cleanup

---
+++ HelloWorld/README.md
@@ -0,0 +1,21 @@
+# Hello, World GameBoy Color Project
+
+![Screenshot](screenshot.png)
+
+## Game Engine
+This project is to create a simple engine and learn GBC processor instructions.
+
+## Tooling
+### Emulation
+- BGB https://bgb.bircd.org/
+
+### Coding
+- RGBDS - https://rgbds.gbdev.io/
+
+### Graphics
+- ProMotion NG - https://www.cosmigo.com/
+- rgbgfx
+- Game Boy Tile Designer - https://www.devrs.com/gb/hmgd/gbtd.html
+
+### Audio
+- hUGETracker - https://nickfa.ro/wiki/hUGETracker
--- HelloWorld/SRC/main.asm
+++ HelloWorld/SRC/main.asm
@@ -5,31 +5,34 @@
 ; https://github.com/w84death/gameboy-color-games
 ; ==========================================================================|80|
 
-; ======================================> REGISTERS DEFINITIONS <===========|80|
+; ======================================> GBC REGISTERS <===================|80|
 INCLUDE "gbc_regs.inc"
 ; ==========================================================================|80|
 
 ; ======================================> GAME CONSTANTS  <=================|80|
-DEF DPAD_RIGHT                              equ $0000
-DEF DPAD_LEFT                               equ $0001
-DEF DPAD_UP                                 equ $0002
-DEF DPAD_DOWN                               equ $0003
+DEF DPAD_RIGHT_BIT                      EQU $0000
+DEF DPAD_LEFT_BIT                       EQU $0001
+DEF DPAD_UP_BIT                         EQU $0002
+DEF DPAD_DOWN_BIT                       EQU $0003
+DEF SPRITE_TILE_ADDR                    EQU $8000
+DEF SPRITE_X_CENTER                     EQU 80 + 8
+DEF SPRITE_Y_CENTER                     EQU 72 + 16
 
 ; ==========================================================================|80|
 
 ; ======================================> HEADEWR <=========================|80|
 SECTION "Header", ROM0[$100]
-    nop
-    jp Entry
-    ds $150 - @, 0
+  nop
+  jp Entry
+  ds $150 - @, 0
 ; ==========================================================================|80|
 
-; ======================================> HEADEWR <=========================|80|
+; ======================================> MAIN SECTION <====================|80|
 SECTION "Main", ROM0[$150]
 
 Entry:
-  di
-  call WaitVBlankNow
+  di                                    ; disable interrups
+  call WaitVBlank
 
   xor a
   ld [rLCDC], a
@@ -41,67 +44,46 @@ Entry:
   ; Clear _OAMRAM
   ld hl, _OAMRAM
   ld b, 160
-
   .clear_oam
     xor a
     ld [hli], a
     dec b
     jr nz, .clear_oam
 
-    ; Copy sprite tile to $8000
-    ld hl, SpriteTile
-    ld de, $8000
-    ld b, SpriteTileEnd - SpriteTile
-  .copy_tile
-    ld a, [hli]
-    ld [de], a
-    inc de
-    dec b
-    jr nz, .copy_tile
+  .load_sprite_tiles
+    ld hl, GameTiles
+    ld de, SPRITE_TILE_ADDR
+    ld bc, GameTilesEnd - GameTiles
+    call LoadBytes
 
-    ; DMG sprite palette: color 1/2/3 visible
-    ld a, %11100100
+  .load_palettes
+    ld a, [DMGObjPalette0]
     ld [rOBP0], a
 
-    ; CGB OBJ palette 0:
-    ; color 0 white, color 1 black, color 2 black, color 3 black
-    ld a, $80
-    ld [rOBPI], a
-    ld a, $FF
-    ld [rOBPD], a
-    ld a, $7F
-    ld [rOBPD], a
+    ld hl, CGBObjPalettes
+    ld b, CGBObjPalettesEnd - CGBObjPalettes
+    call LoadObjPalettesCGB
+
+  .draw_player
+    ld hl, _OAMRAM
+    ld a, SPRITE_Y_CENTER
+    ld [hli], a
+    ld a, SPRITE_X_CENTER
+    ld [hli], a
     xor a
-    ld [rOBPD], a
-    ld [rOBPD], a
-    ld [rOBPD], a
-    ld [rOBPD], a
-    ld [rOBPD], a
-    ld [rOBPD], a
+    ld [hli], a
+    ld [hl], a         ; attrs: CGB OBJ palette 0, VRAM bank 0
 
-    call CenterSprite
-
-    ; LCD on, sprites on
-    ld a, $80 | $02
-    ld [rLCDC], a
+  .turn_lcd_on
+  ; LCD on, sprites on
+  ld a, $80 | $02
+  ld [rLCDC], a
 
 ; ======================================> MAIN LOOP <=======================|80|
 MainLoop:
-  call WaitVBlankNow
-  call ReadDPad
-  bit 0, a
-  call z, MoveRight
-
-  bit 1, a
-  call z, MoveLeft
-
-  bit 2, a
-  call z, MoveUp
-
-  bit 3, a
-  call z, MoveDown
-
-  call WaitVBlankEnd
+  call WaitVBlank
+  call HandleDPad
+  call WaitVBlank
   jr MainLoop
 
 ; ==========================================================================|80|
@@ -109,16 +91,90 @@ MainLoop:
 
 
 ; ======================================> PROCEDURES <======================|80|
-WaitVBlankNow:
+WaitVBlank:
+  .wait_vblank
   ld a, [rLY]
   cp 144
-  jr c, WaitVBlankNow
+  jr c, .wait_vblank
+
+  .wait_vblank_end
+    ld a, [rLY]
+    cp 144
+    jr nc, .wait_vblank_end
+
   ret
 
-WaitVBlankEnd:
-  ld a, [rLY]
-  cp 144
-  jr nc, WaitVBlankEnd
+LoadBytes:
+  ; Copies BC bytes from HL to DE
+  ; input:
+  ;   HL = source
+  ;   DE = destination
+  ;   BC = byte count
+.copy
+  ld a, b
+  or c
+  ret z
+
+  ld a, [hli]
+  ld [de], a
+  inc de
+  dec bc
+  jr .copy
+ret
+
+LoadObjPalettesCGB:
+  ; Loads CGB OBJ palette bytes using auto-increment.
+  ; input:
+  ;   HL = palette data
+  ;   B  = byte count
+  ;
+  ; Supports multiple palettes packed together.
+  ; 1 palette = 8 bytes = 4 colors.
+
+  ld a, $80
+  ld [rOBPI], a
+
+.copy
+  ld a, b
+  or a
+  ret z
+
+  ld a, [hli]
+  ld [rOBPD], a
+  dec b
+  jr .copy
+ret
+
+LoadObjPaletteCGB:
+  ; Loads CGB OBJ palette bytes using auto-increment
+  ; input:
+  ;   HL = palette data
+  ;   B  = byte count
+  ld a, $80
+  ld [rOBPI], a
+
+.copy
+  ld a, b
+  or a
+  ret z
+
+  ld a, [hli]
+  ld [rOBPD], a
+  dec b
+  jr .copy
+ret
+
+
+HandleDPad:
+  call ReadDPad
+  bit DPAD_RIGHT_BIT, a
+  call z, MoveRight
+  bit DPAD_LEFT_BIT, a
+  call z, MoveLeft
+  bit DPAD_UP_BIT, a
+  call z, MoveUp
+  bit DPAD_DOWN_BIT, a
+  call z, MoveDown
   ret
 
 ReadDPad:
@@ -150,27 +206,36 @@ MoveDown:
   ret
 ; ==========================================================================|80|
 
-CenterSprite:
-  ; Sprite centered roughly at screen x=80, y=72
-  ld hl, _OAMRAM
-  ld a, 72 + 16
-  ld [hli], a
-  ld a, 80 + 8
-  ld [hli], a
-  xor a
-  ld [hli], a        ; tile 0
-  ld [hl], a         ; attrs: palette 0, VRAM bank 0
-  ret
-
 ; ======================================> GAME DATA <=======================|80|
-SpriteTile:
-  db %00111100, %00000000
-  db %01000010, %00000000
-  db %10100101, %00000000
-  db %10000001, %00000000
-  db %10100101, %00000000
-  db %10011001, %00000000
-  db %01000010, %00000000
-  db %00111100, %00000000
-SpriteTileEnd:
+DMGObjPalette0:
+  db %11100100
+
+CGBObjPalettes:
+CGBObjPalette0:
+  dw $7FFF
+  dw $22E7
+  dw $19C4
+  dw $14E0
+CGBObjPalette0End:
+
+; Palette used by tiles 0 and 1
+CGBObjPalette1:
+  dw $7FFF
+  dw $7D7D
+  dw $221F
+  dw $3501
+CGBObjPalette1End:
+CGBObjPalettesEnd:
+
+GameTiles:
+TilePlayerA:
+  DB $00,$00,$42,$00,$18,$24,$66,$18
+  DB $3C,$66,$42,$3C,$66,$18,$3C,$24
+TilePlayerB:
+  DB $00,$00,$99,$24,$66,$18,$3C,$66
+  DB $42,$3C,$24,$18,$18,$24,$24,$24
+TileTree:
+  DB $38,$46,$68,$96,$E4,$1B,$6E,$91
+  DB $D8,$27,$90,$6F,$00,$3E,$18,$18
+GameTilesEnd:
 ; ==========================================================================|80|
