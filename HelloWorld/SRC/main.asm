2148b81ce0734e75c23e9b1a7ef85fc180893688
Krzysztof Krystian Jankowski <kj@p1x.in>
2026-05-05 12:33:57 +0200

    code cleanup

--- HelloWorld/SRC/main.asm
+++ HelloWorld/SRC/main.asm
@@ -1,34 +1,48 @@
-; main.asm
+; ==========================================================================|80|
+; Hello, World GameBoy Color Game
+; by Krzysztof Krystian Jankowski
+; smol.p1x.in/assembly/
+; https://github.com/w84death/gameboy-color-games
+; ==========================================================================|80|
 
+; ======================================> REGISTERS DEFINITIONS <===========|80|
 INCLUDE "gbc_regs.inc"
+; ==========================================================================|80|
 
+; ======================================> GAME CONSTANTS  <=================|80|
+DEF DPAD_RIGHT                              equ $0000
+DEF DPAD_LEFT                               equ $0001
+DEF DPAD_UP                                 equ $0002
+DEF DPAD_DOWN                               equ $0003
+
+; ==========================================================================|80|
+
+; ======================================> HEADEWR <=========================|80|
 SECTION "Header", ROM0[$100]
     nop
     jp Entry
     ds $150 - @, 0
+; ==========================================================================|80|
 
+; ======================================> HEADEWR <=========================|80|
 SECTION "Main", ROM0[$150]
 
 Entry:
-    di
+  di
+  call WaitVBlankNow
 
-WaitVBlank:
-    ld a, [rLY]
-    cp 144
-    jr c, WaitVBlank
+  xor a
+  ld [rLCDC], a
 
-    xor a
-    ld [rLCDC], a
+  ; VRAM bank 0
+  xor a
+  ld [rVBK], a
 
-    ; VRAM bank 0
-    xor a
-    ld [rVBK], a
+  ; Clear _OAMRAM
+  ld hl, _OAMRAM
+  ld b, 160
 
-    ; Clear _OAMRAM
-    ld hl, _OAMRAM
-    ld b, 160
-
-.clear_oam
+  .clear_oam
     xor a
     ld [hli], a
     dec b
@@ -38,7 +52,7 @@ WaitVBlank:
     ld hl, SpriteTile
     ld de, $8000
     ld b, SpriteTileEnd - SpriteTile
-.copy_tile
+  .copy_tile
     ld a, [hli]
     ld [de], a
     inc de
@@ -65,92 +79,98 @@ WaitVBlank:
     ld [rOBPD], a
     ld [rOBPD], a
 
-    ; Sprite centered roughly at screen x=80, y=72
-    ld hl, _OAMRAM
-    ld a, 72 + 16
-    ld [hli], a
-    ld a, 80 + 8
-    ld [hli], a
-    xor a
-    ld [hli], a        ; tile 0
-    ld [hl], a         ; attrs: palette 0, VRAM bank 0
+    call CenterSprite
 
     ; LCD on, sprites on
     ld a, $80 | $02
     ld [rLCDC], a
 
-Loop:
+; ======================================> MAIN LOOP <=======================|80|
+MainLoop:
   call WaitVBlankNow
   call ReadDPad
+  bit 0, a
+  call z, MoveRight
 
-   ; A now contains d-pad bits:
-   ; bit 0 = Right
-   ; bit 1 = Left
-   ; bit 2 = Up
-   ; bit 3 = Down
-   ; pressed = 0
+  bit 1, a
+  call z, MoveLeft
 
-   bit 0, a
-   call z, MoveRight
+  bit 2, a
+  call z, MoveUp
 
-   bit 1, a
-   call z, MoveLeft
+  bit 3, a
+  call z, MoveDown
 
-   bit 2, a
-   call z, MoveUp
+  call WaitVBlankEnd
+  jr MainLoop
 
-   bit 3, a
-   call z, MoveDown
+; ==========================================================================|80|
 
-   call WaitVBlankEnd
-   jr Loop
 
-   WaitVBlankNow:
-       ld a, [rLY]
-       cp 144
-       jr c, WaitVBlankNow
-       ret
 
-   WaitVBlankEnd:
-       ld a, [rLY]
-       cp 144
-       jr nc, WaitVBlankEnd
-       ret
+; ======================================> PROCEDURES <======================|80|
+WaitVBlankNow:
+  ld a, [rLY]
+  cp 144
+  jr c, WaitVBlankNow
+  ret
+
+WaitVBlankEnd:
+  ld a, [rLY]
+  cp 144
+  jr nc, WaitVBlankEnd
+  ret
+
 ReadDPad:
-       ld a, %00100000     ; select d-pad buttons
-       ld [rP1], a
-       ld a, [rP1]
-       ld a, [rP1]         ; read twice for stability
-       and %00001111
-       ret
+  ld a, %00100000     ; select d-pad buttons
+  ld [rP1], a
+  ld a, [rP1]
+  ld a, [rP1]         ; read twice for stability
+  and %00001111       ; clean hiher bits to leave only buttons information
+  ret
 
-   MoveRight:
-       ld hl, _OAMRAM + 1  ; sprite X
-       inc [hl]
-       ret
+MoveRight:
+  ld hl, _OAMRAM + 1  ; sprite X
+  inc [hl]
+  ret
 
-   MoveLeft:
-       ld hl, _OAMRAM + 1
-       dec [hl]
-       ret
+MoveLeft:
+  ld hl, _OAMRAM + 1
+  dec [hl]
+  ret
 
-   MoveUp:
-       ld hl, _OAMRAM      ; sprite Y
-       dec [hl]
-       ret
+MoveUp:
+  ld hl, _OAMRAM      ; sprite Y
+  dec [hl]
+  ret
 
-   MoveDown:
-       ld hl, _OAMRAM
-       inc [hl]
-       ret
+MoveDown:
+  ld hl, _OAMRAM
+  inc [hl]
+  ret
+; ==========================================================================|80|
 
+CenterSprite:
+  ; Sprite centered roughly at screen x=80, y=72
+  ld hl, _OAMRAM
+  ld a, 72 + 16
+  ld [hli], a
+  ld a, 80 + 8
+  ld [hli], a
+  xor a
+  ld [hli], a        ; tile 0
+  ld [hl], a         ; attrs: palette 0, VRAM bank 0
+  ret
+
+; ======================================> GAME DATA <=======================|80|
 SpriteTile:
-    db %00111100, %00000000
-    db %01000010, %00000000
-    db %10100101, %00000000
-    db %10000001, %00000000
-    db %10100101, %00000000
-    db %10011001, %00000000
-    db %01000010, %00000000
-    db %00111100, %00000000
+  db %00111100, %00000000
+  db %01000010, %00000000
+  db %10100101, %00000000
+  db %10000001, %00000000
+  db %10100101, %00000000
+  db %10011001, %00000000
+  db %01000010, %00000000
+  db %00111100, %00000000
 SpriteTileEnd:
+; ==========================================================================|80|
